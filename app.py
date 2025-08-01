c44a37f0ff043dba185c83b5d8e32994816c70b9
wmo600 <woo197588@gmail.com>
2025-08-01 20:24:08 +0800

    adding mood_gifs later

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 # Admin Number: 2334369
 # Class: DIT/FT/3A/51
 
+import warnings
 import streamlit as st
+from components.mood_gifs import get_mood_gif
 from components.sidebar import render_sidebar
 from helpers.theme_helper import init_theme
 from components.mood_input import get_user_mood
@@ -10,6 +12,7 @@ from components.recommendations import show_recommendations
 from helpers.youtube_helper import search_youtube
 from helpers.mood_history_helper import save_mood_entry
 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 st.set_page_config(page_title="Home", page_icon="🏠")
 
@@ -24,6 +27,7 @@ final_mood, user_input, input_method = get_user_mood()
 st.markdown("### 🎵 Recommended Song for You")
 
 if final_mood != "unknown":
+    get_mood_gif(final_mood)
     search_query = (
         f'{final_mood} music playlist for when you feel "{user_input}"'
         if input_method == "Type my feeling"
